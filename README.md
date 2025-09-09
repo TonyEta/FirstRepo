4f147710778e776dcaff7d931c037cd1077d58c2
Max <shamruk01@gmail.com>
2025-09-09 21:31:44 +0300

    Lesson 1 work plan updates

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 1. Changing files
 2. Committing files
 3. Pushing to the server
+4. Doing nothing
--- main.py
+++ main.py
@@ -2,4 +2,4 @@ pairs = ['name:max', 'age:10']
 
 my_dict = {i.split(':')[0]: i.split(':')[1]for i in pairs}
 
-print(my_dict)
+print('my dict -> ', my_dict)
